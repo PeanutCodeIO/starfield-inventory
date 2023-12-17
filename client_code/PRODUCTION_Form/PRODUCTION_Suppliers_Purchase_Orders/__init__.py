214f55441f764e8eb59ccafb544acb47ae6a089b
doubleornothingcoffee@gmail.com <doubleornothingcoffee@gmail.com>
2023-12-17 09:37:16 +0000

    Created PRODUCTION_Form.PRODUCTION_Suppliers_Purchase_Orders; Edited PRODUCTION_Form.PRODUCTION_Suppliers_Module and LOGIN_Form

---
+++ client_code/PRODUCTION_Form/PRODUCTION_Suppliers_Purchase_Orders/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import PRODUCTION_Suppliers_Purchase_OrdersTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class PRODUCTION_Suppliers_Purchase_Orders(PRODUCTION_Suppliers_Purchase_OrdersTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
