74113bb8c98d5904b852004c380021199099268e
doubleornothingcoffee@gmail.com <doubleornothingcoffee@gmail.com>
2023-12-17 10:02:34 +0000

    Created PRODUCTION_Form.PRODUCTION_Suppliers_Purchase_Orders.New_Purchase_Orders and PRODUCTION_Form.PRODUCTION_Suppliers_Purchase_Orders.Purchase_Orders; Edited PRODUCTION_Form.PRODUCTION_Suppliers_Module and PRODUCTION_Form.PRODUCTION_Suppliers_Purchase_Orders

--- client_code/PRODUCTION_Form/PRODUCTION_Suppliers_Purchase_Orders/__init__.py
+++ client_code/PRODUCTION_Form/PRODUCTION_Suppliers_Purchase_Orders/__init__.py
@@ -7,10 +7,20 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .Purchase_Orders import Purchase_Orders
 
 class PRODUCTION_Suppliers_Purchase_Orders(PRODUCTION_Suppliers_Purchase_OrdersTemplate):
-  def __init__(self, **properties):
+  def __init__(self,supplier_id = None, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.supplier_id = supplier_id
 
     # Any code you write here will run before the form opens.
+    cmpt = Purchase_Orders()
+    self.content_panel.clear()
+    self.content_panel.add_component(cmpt)
+    
+
+  def new_po_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
